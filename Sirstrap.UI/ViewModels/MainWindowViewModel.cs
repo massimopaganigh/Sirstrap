1eb7bf9cd50526b0a360492291e27b12b00ba059
mpagani <massimo.pagani@unitec-group.com>
2025-03-03 12:39:36 +0100

    feat: Last log display

--- Sirstrap.UI/ViewModels/MainWindowViewModel.cs
+++ Sirstrap.UI/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Serilog;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Serilog;
+using Serilog.Events;
 using Sirstrap.Core;
 using System;
 using System.Linq;
@@ -15,23 +17,62 @@ namespace Sirstrap.UI.ViewModels
     /// - Configuring and initializing the Serilog logger
     /// - Registering the Roblox protocol handler
     /// - Initiating the Roblox download process
+    /// - Exposing the last log message for the UI
     /// </para>
     /// <para>
     /// The view model starts these operations asynchronously in a background thread upon instantiation.
     /// </para>
     /// </remarks>
-    public partial class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ObservableObject
     {
+        /// <summary>
+        /// Gets or sets the most recent log message from the application.
+        /// </summary>
+        [ObservableProperty]
+        private string _lastLogMessage = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the timestamp of the last log message.
+        /// </summary>
+        [ObservableProperty]
+        private DateTimeOffset? _lastLogTimestamp;
+
+        /// <summary>
+        /// Gets or sets the level of the last log message.
+        /// </summary>
+        [ObservableProperty]
+        private LogEventLevel? _lastLogLevel;
+
+        private readonly System.Timers.Timer _logPollingTimer;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
         /// </summary>
         /// <remarks>
         /// This constructor launches the main application logic asynchronously in a background thread
-        /// to avoid blocking the UI thread during initialization.
+        /// to avoid blocking the UI thread during initialization. It also sets up a timer to poll
+        /// for log updates from the LastLogSink.
         /// </remarks>
         public MainWindowViewModel()
         {
+            _logPollingTimer = new System.Timers.Timer(100);
+            _logPollingTimer.Elapsed += (s, e) => UpdateLastLogFromSink();
+            _logPollingTimer.Start();
+
             Task.Run(() => Main(Environment.GetCommandLineArgs()));
+        }
+
+        /// <summary>
+        /// Updates the observable properties with the latest log information from LastLogSink.
+        /// </summary>
+        private void UpdateLastLogFromSink()
+        {
+            if (LastLogMessage != LastLogSink.LastLog)
+            {
+                LastLogMessage = LastLogSink.LastLog;
+                LastLogTimestamp = LastLogSink.LastLogTimestamp;
+                LastLogLevel = LastLogSink.LastLogLevel;
+            }
         }
 
         /// <summary>
@@ -59,7 +100,7 @@ namespace Sirstrap.UI.ViewModels
         {
             try
             {
-                Log.Logger = new LoggerConfiguration().WriteTo.File("SirstrapLog.txt").CreateLogger();
+                Log.Logger = new LoggerConfiguration().WriteTo.File("SirstrapLog.txt").WriteTo.LastLog().CreateLogger();
                 RegistryManager.RegisterProtocolHandler("roblox-player");
 
                 var filteredArgs = args.Skip(1).ToArray();
@@ -72,6 +113,15 @@ namespace Sirstrap.UI.ViewModels
 
                 Environment.Exit(0);
             }
+        }
+
+        /// <summary>
+        /// Performs cleanup when the view model is being disposed.
+        /// </summary>
+        public void Dispose()
+        {
+            _logPollingTimer?.Stop();
+            _logPollingTimer?.Dispose();
         }
     }
 }
