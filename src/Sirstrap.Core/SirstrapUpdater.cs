b8069be538dd7a31516ad7a78c0461a9534c5ab7
Massimo <mp120805a@gmail.com>
2025-05-26 22:37:53 +0200

    version: Release 1.1.6.2

--- src/Sirstrap.Core/SirstrapUpdater.cs
+++ src/Sirstrap.Core/SirstrapUpdater.cs
@@ -195,7 +195,7 @@ exit
         /// <returns>The current version of Sirstrap.</returns>
         private static Version GetCurrentVersion()
         {
-            return new Version("1.1.6.1");
+            return new Version("1.1.6.2");
         }
 
         /// <summary>
