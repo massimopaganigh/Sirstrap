61bf4cb92b85071e59c98d4883bda1ae32455eb0
Massimo <mp120805a@gmail.com>
2025-06-19 00:12:06 +0200

    fix: Release Build workflow

--- build/release.bat
+++ build/release.bat
@@ -3,14 +3,14 @@
 setlocal enabledelayedexpansion
 
 set "sybau=%1"
+set "wingetcreate_path=..\src\ext\Microsoft.WindowsPackageManagerManifestCreator_1.9.14.0\wingetcreate.exe"
 
 for /f "usebackq delims=" %%i in ("..\VERSION") do set "version_raw=%%i"
 
 set "version=%version_raw:v=%"
 set "version=%version:-beta=%"
 
-winget install wingetcreate
-wingetcreate.exe update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/sirstrap/releases/download/%version_raw%/Sirstrap.CLI_fat.zip" --version %version% Sirstrap.CLI
-wingetcreate.exe update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/sirstrap/releases/download/%version_raw%/Sirstrap.UI_fat.zip" --version %version% Sirstrap.UI
+%wingetcreate_path% update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/sirstrap/releases/download/%version_raw%/Sirstrap.CLI_fat.zip" --version %version% Sirstrap.CLI
+%wingetcreate_path% update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/sirstrap/releases/download/%version_raw%/Sirstrap.UI_fat.zip" --version %version% Sirstrap.UI
 
 endlocal
