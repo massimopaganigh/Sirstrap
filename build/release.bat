549f25d3af905fd0e55d0e2f9cb0ca9791729e15
Massimo <mp120805a@gmail.com>
2025-06-19 00:25:43 +0200

    enhance: Better release batch

--- build/release.bat
+++ build/release.bat
@@ -10,7 +10,22 @@ for /f "usebackq delims=" %%i in ("..\VERSION") do set "version_raw=%%i"
 set "version=%version_raw:v=%"
 set "version=%version:-beta=%"
 
+echo Releasing Sirstrap.CLI...
+
 %wingetcreate_path% update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/sirstrap/releases/download/%version_raw%/Sirstrap.CLI_fat.zip" --version %version% Sirstrap.CLI
+
+if %ERRORLEVEL% neq 0 (
+    echo Release of Sirstrap.CLI failed.
+    exit /b %ERRORLEVEL%
+)
+
+echo Releasing Sirstrap.UI...
+
 %wingetcreate_path% update --submit --token "%sybau%" --urls "https://github.com/massimopaganigh/sirstrap/releases/download/%version_raw%/Sirstrap.UI_fat.zip" --version %version% Sirstrap.UI
+
+if %ERRORLEVEL% neq 0 (
+    echo Release of Sirstrap.UI failed.
+    exit /b %ERRORLEVEL%
+)
 
 endlocal
