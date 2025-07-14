52d6b536a20c1170dfb5d2a87188c2a9daf6b4c7
mpagani <Massimo.Pagani@unitec-group.com>
2025-07-14 10:53:53 +0200

    enhance: cabinet file format for releases

--- build/post_build.bat
+++ build/post_build.bat
@@ -3,6 +3,7 @@
 setlocal enabledelayedexpansion
 
 set "release_dir=..\out\release"
+set "sircab_path=..\src\ext\SirCab\SirCab.exe"
 set "sirstrap_cli_publish_dir=..\out\Sirstrap.CLI"
 set "sirstrap_cli_fat_publish_dir=..\out\Sirstrap.CLI_fat"
 set "sirstrap_ui_publish_dir=..\out\Sirstrap.UI"
@@ -26,6 +27,15 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 
+echo Archiving Sirstrap.CLI - SirCab...
+
+"%sircab_path%" "%sirstrap_cli_publish_dir%" "%release_dir%" "Sirstrap.CLI" "LZX"
+
+if %ERRORLEVEL% neq 0 (
+    echo Archiving of Sirstrap.CLI failed - SirCab.
+    exit /b %ERRORLEVEL%
+)
+
 echo Archiving Sirstrap.UI_fat...
 
 powershell Compress-Archive -Path "%sirstrap_ui_fat_publish_dir%\*" -DestinationPath "%sirstrap_ui_fat_publish_dir%.zip" -Force
@@ -44,13 +54,24 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 
-echo Moving archives...
+echo Archiving Sirstrap.UI - SirCab...
 
-mkdir "%release_dir%"
+"%sircab_path%" "%sirstrap_ui_publish_dir%" "%release_dir%" "Sirstrap.UI" "LZX"
+
+if %ERRORLEVEL% neq 0 (
+    echo Archiving of Sirstrap.UI failed - SirCab.
+    exit /b %ERRORLEVEL%
+)
+
+echo Moving archives...
 
 move /y "%sirstrap_cli_publish_dir%.zip" "%release_dir%\Sirstrap.CLI.zip"
 move /y "%sirstrap_cli_fat_publish_dir%.zip" "%release_dir%\Sirstrap.CLI_fat.zip"
 move /y "%sirstrap_ui_publish_dir%.zip" "%release_dir%\Sirstrap.UI.zip"
 move /y "%sirstrap_ui_fat_publish_dir%.zip" "%release_dir%\Sirstrap.UI_fat.zip"
+
+echo Cleaning files...
+
+del /f /q "%release_dir%\*.ddf"
 
 endlocal
