06a47c1350c40ee8e32f8d262be3100ecd22975b
ギャップ <mp120805a@gmail.com>
2025-08-20 13:26:43 +0200

    refactor: `Installer`

---
+++ src/Sirstrap.Core/Extensions/StringExtension.cs
@@ -0,0 +1,62 @@
+﻿namespace Sirstrap.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static void BetterDirectoryCreate(this string directoryPath, int attempts = 5)
+        {
+            foreach (int attempt in Enumerable.Range(1, attempts))
+                try
+                {
+                    if (!Directory.Exists(directoryPath))
+                        Directory.CreateDirectory(directoryPath);
+
+                    return;
+                }
+                catch (IOException)
+                {
+                    if (attempt == attempts)
+                        throw;
+
+                    Thread.Sleep(100 * attempt);
+                }
+        }
+
+        public static void BetterDirectoryDelete(this string directoryPath, int attempts = 5)
+        {
+            foreach (int attempt in Enumerable.Range(1, attempts))
+                try
+                {
+                    if (Directory.Exists(directoryPath))
+                        Directory.Delete(directoryPath, true);
+
+                    return;
+                }
+                catch (IOException)
+                {
+                    if (attempt == attempts)
+                        throw;
+
+                    Thread.Sleep(100 * attempt);
+                }
+        }
+
+        public static void BetterFileDelete(this string filePath, int attempts = 5)
+        {
+            foreach (int attempt in Enumerable.Range(1, attempts))
+                try
+                {
+                    if (File.Exists(filePath))
+                        File.Delete(filePath);
+
+                    return;
+                }
+                catch (IOException)
+                {
+                    if (attempt == attempts)
+                        throw;
+
+                    Thread.Sleep(100 * attempt);
+                }
+        }
+    }
+}
