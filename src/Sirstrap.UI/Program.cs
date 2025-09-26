bf0d7a6649e9f3f32044f72ced801c47c8584bcc
ギャップ <mp120805a@gmail.com>
2025-09-26 08:14:28 +0200

    fix: startup

--- src/Sirstrap.UI/Program.cs
+++ src/Sirstrap.UI/Program.cs
@@ -4,7 +4,7 @@
     {
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool SetBinDirectory(string binDirectoryPath);
+        private static extern bool SetDllDirectory(string lpPathName);
 
         public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont().LogToTrace();
 
@@ -14,7 +14,7 @@
             var binDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin");
 
             if (Directory.Exists(binDirectoryPath))
-                SetBinDirectory(binDirectoryPath);
+                SetDllDirectory(binDirectoryPath);
 
             Args = args.Length > 0 ? args : null;
 
