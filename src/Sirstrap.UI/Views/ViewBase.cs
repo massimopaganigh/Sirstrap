25b7e441bbd3a21d5935a758c0801843c105eb62
massimopaganigh <mp120805a@gmail.com>
2025-10-20 19:42:12 +0200

    fix: opacity issues and partial revert

--- src/Sirstrap.UI/Views/ViewBase.cs
+++ src/Sirstrap.UI/Views/ViewBase.cs
@@ -31,7 +31,7 @@
                         scaleTransform.ScaleY = 1.0 - (0.9 * easedProgress);
                     }
 
-                    Opacity = 1 * (1 - progress);
+                    Opacity = 0.5 * (1 - progress);
                 });
 
                 await Task.Delay(stepDelay);
@@ -57,7 +57,7 @@
                         scaleTransform.ScaleY = 0.1 + (0.9 * easedProgress);
                     }
 
-                    Opacity = 1 * progress;
+                    Opacity = 0.5 * progress;
                 });
 
                 await Task.Delay(stepDelay);
